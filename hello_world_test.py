39abed7141da765cfc7cb734b36393a5a0eb7313
Scott Field <sfield13@ivytech.edu>
2023-03-26 01:47:48 -0700

    added hello_world_test.py (prints hello world)

---
+++ hello_world_test.py
@@ -0,0 +1,10 @@
+'''
+Author: Scott Field
+"Version: 1.00
+"Date: 3/21/2023
+"Program Name: hello_world_test
+"Program Purpose: print hello world in a program that has been put to a github repository using a command line interface.
+'''
+    
+#print statement"
+print("Hello World")
